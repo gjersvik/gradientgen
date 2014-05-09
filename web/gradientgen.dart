4fb1e64ac2f23a49dd2d0aa1670420a58ba07241
Ole Martin Gjersvik <olem@olem.org>
2014-05-09 17:06:34 +0200

    Paint first gradient.

--- web/gradientgen.dart
+++ web/gradientgen.dart
@@ -1,12 +1,44 @@
 import 'dart:html';
 import 'dart:math';
 
+var random = new Random();
+
 void main() {
-  var random = new Random();
   CanvasElement canvas = querySelector('canvas');
   CanvasRenderingContext2D paint = canvas.getContext('2d');
   
   // canvas to fullscren
-  canvas.height = document.body.clientHeight;
-  canvas.width = document.body.clientWidth;
+  var height = canvas.height = document.body.clientHeight;
+  var width = canvas.width = document.body.clientWidth;
+  
+  var grad = paint.createLinearGradient(
+      random.nextDouble()*width, random.nextDouble()*height,
+      random.nextDouble()*width, random.nextDouble()*height);
+  
+  grad.addColorStop(random.nextDouble(), genColor());
+  grad.addColorStop(random.nextDouble(), genColor());
+  grad.addColorStop(random.nextDouble(), genColor());
+  grad.addColorStop(random.nextDouble(), genColor());
+  grad.addColorStop(random.nextDouble(), genColor());
+  grad.addColorStop(random.nextDouble(), genColor());
+  grad.addColorStop(random.nextDouble(), genColor());
+  
+  paint.fillStyle = grad;
+  paint.fillRect(0, 0, width, height);
 }
+
+String genColor(){
+  var r = (random.nextDouble() * 256).floor();
+  var g = (random.nextDouble() * 256).floor();
+  var b = (random.nextDouble() * 256).floor();
+  return 'rgb($r,$g,$b)';
+}
+
+String genColorAlfa(){
+  var r = (random.nextDouble() * 256).floor();
+  var g = (random.nextDouble() * 256).floor();
+  var b = (random.nextDouble() * 256).floor();
+  var a = random.nextDouble();
+  return 'rgba($r,$g,$b,${a.toStringAsFixed(5)})';
+}
+
