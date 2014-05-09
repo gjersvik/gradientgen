a761600e5c1f5978b9479a84102df4bb4efa546b
Ole Martin Gjersvik <olem@olem.org>
2014-05-09 16:22:26 +0200

    Cnavas resize.

--- web/gradientgen.dart
+++ web/gradientgen.dart
@@ -1,5 +1,12 @@
 import 'dart:html';
+import 'dart:math';
 
 void main() {
+  var random = new Random();
+  CanvasElement canvas = querySelector('canvas');
+  CanvasRenderingContext2D paint = canvas.getContext('2d');
   
+  // canvas to fullscren
+  canvas.height = document.body.clientHeight;
+  canvas.width = document.body.clientWidth;
 }
